1b1d74ef7112d28af64c541b84506335c55433b1
RooD7 <rodrigosoualves7@gmail.com>
2018-11-06 17:52:35 -0200

    Semantico init

--- Semantico.py
+++ Semantico.py
@@ -1,3 +1,25 @@
 '''
 	Analisador Semantico
 '''
+
+'''
+ for id temp2:
+ 	if id in tabSimb:
+ 		"erro"
+ 	else:
+ 		tabSimb[id] = (tosp1, integer(0))
+'''
+
+class ErroSemantico(Exception):
+	"""docstring for ErroSemantico"""
+	def __init__(self, tk):
+		self.token = tk
+
+	def __str__(self):
+		return "ERRO: Semantico\n"
+
+class TabSimb:
+	pass
+
+class Semantico:
+	pass
